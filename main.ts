afe1a2aafa45d2c9e3b13dde34b90d1db999ef2d
JaroslavNosek <jaroslav.nosek.024@pslib.cz>
2025-02-17 10:03:57 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,41 @@
-let stopTime = 0
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (receivedNumber === 9) {
+        console.log("restart")
+        control.reset()
+    }
 
-Sensors.SetLightLevel()
+})
+    
 
 radio.setGroup(92)
 
-radio.onReceivedNumber(function(receivedNumber: number) {
+let stopTime = 0
+let startTime = 0
+
+Sensors.SetLightLevel()
+
+
+    
+
+
+radio.onReceivedNumber(function (receivedNumber: number) {
     if (receivedNumber === 1) {
-        music.playTone(850, 1000)
+        startTime = input.runningTime()  
+        basic.showString("!")
+        console.log("Start")
     }
 })
 
 
-Sensors.OnLightDrop(function() {
-    music.playTone(300, 1000)
+Sensors.OnLightDrop(function () {
+    music.playTone(500, 500)
+    stopTime = input.runningTime()  
+    let raceTime = (stopTime - startTime) / 1000  
+    console.log(raceTime)
+    basic.showNumber(raceTime)
+    radio.sendNumber(raceTime)
+        })
 
 
-})
+
+    
